b35ec30ee614c800bb6ffea866a661f296177a1c
Mordechai Fast <mordechai.fast@gmail.com>
2022-01-25 00:53:36 +0200

    fixed password reset

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from users.views import dashboard, register
 
 urlpatterns = [
+    path('', dashboard),
     path('dashboard/', dashboard, name='dashboard'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', register, name='register'),
