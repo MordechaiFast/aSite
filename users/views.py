30de8608116da2a1457e7b4d0ad72e5a77cde0a0
Mordechai Fast <mordechai.fast@gmail.com>
2022-01-25 00:37:58 +0200

    Register new user

--- users/views.py
+++ users/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users.forms import UserCreationFormEMail
+from django.contrib.auth import login
+from django.urls import reverse
 
 def dashboard(request):
     return render(request, 'users/dashboard.html')
+
+def register(request):
+    if request.method == "GET":
+        return render(request, 'users/register.html',
+         {'form': UserCreationFormEMail})
+    elif request.method == "POST":
+        form = UserCreationFormEMail(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse('dashboard'))
